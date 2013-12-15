568042c4cd862a719c66c79d31739bb9a8c6b484
Matt Thees <mthees7@gmail.com>
2013-12-14 21:05:41 -0500

    icons working and sidebar scrolling

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,10 @@
-//= require_tree .
+// require_tree .
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > $(window).height()) {
+      $("#navigation").addClass("sticky");
+    } else {
+      $("#navigation").removeClass("sticky");
+    }
+
+});
